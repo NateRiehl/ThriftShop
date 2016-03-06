a8bead3f646d556b297c7e36b3941f0ef13c0947
Nate <riehna01@gettysburg.edu>
2016-03-06 09:41:41 -0500

    Profile page updates..

--- webpages/profile.php
+++ webpages/profile.php
@@ -22,15 +22,25 @@
 	<button type='submit' onclick=location.href='#';> Change Profile Pic </button>
 
 	</DIV>
-
+	<DIV class = "name">
+		<h1> John Doe </h1>
+		
+	</DIV>
+	<button type='submit'  id="addItem" onclick=location.href='additem.php';> Add Item </button>
 	<DIV class = "bio">
+	<h1> About Me </h1>
+	<?php
+		$email = $_SESSION['email'];
+		$query = "SELECT bio FROM USER WHERE email = '$email'";
+		$result = $db->query($query);
+		$row = $result->fetch(); 
+		$bio=$row['bio'];
+		printf("<p> %s </p>", $bio);
+	?>
 	<button type='submit'  onclick=location.href='#';> Edit Bio </button>	
 	</DIV>
 	
-	<button type='submit'  id="addItem" onclick=location.href='additem.php';> Add an Item </button>
+	
 
-	<DIV class="reviews">
-		
-	</DIV>
 </BODY>
 </HTML>
